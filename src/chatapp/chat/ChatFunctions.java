6d87522069e9547fe7a78ddd87ab6574bbc1070b
Rajat Patel <admin@technotravel.in>
2013-04-01 22:56:22 +0530

    Update ChatFunctions.java

--- src/chatapp/chat/ChatFunctions.java
+++ src/chatapp/chat/ChatFunctions.java
@@ -11,130 +11,37 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author Rajat
  */
-final class ChatFunctions {
-    OpenServerChat osc;
-    OpenClientChat occ;
-    Socket client_socket;//updated from socket
-    Socket server_socket;//updated from socket
-    PrintWriter toServer;
-    PrintWriter toClient;
+public class ChatFunctions {
     ServerSocket serverSocket;
-    BufferedReader fromServer;
-    BufferedReader fromClient;
-    ChatFunctions(String a) throws IOException{
-        switch (a) {
-            case "Server":
-                //osc =  new OpenServerChat();
-                openServerStreams();
-                break;
-            case "Client":
-                //occ = new OpenClientChat();
-                openClientStreams();
-                break;
-        }
+    Socket client_socket;
+    PrintWriter toClient, toServer;
+    BufferedReader fromClient, fromServer;
+    
+    public void openServerStreams(Socket socket,int port) throws IOException{
+        toClient = new PrintWriter(socket.getOutputStream(),true);
+        fromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        System.out.println("Connected : "+ socket);
     }
-    public void openClientStreams() throws UnknownHostException, IOException{
-        client_socket = new Socket("127.0.0.1",3000);//a change here
-        toServer = new PrintWriter(client_socket.getOutputStream(),true);
-        fromServer = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
-    }
-    public void closeClientStreams() throws IOException{
-        fromServer.close();
-        toServer.close();
-        client_socket.close();//a change here
-    }
-    public void openServerStreams() throws IOException{
-        serverSocket = new ServerSocket(3000);
-        server_socket = serverSocket.accept();//a change here
-        toClient = new PrintWriter(server_socket.getOutputStream(),true);
-        fromClient = new BufferedReader(new InputStreamReader(server_socket.getInputStream()));
-    }
-    public void closeServerStreams() throws IOException{
+    public void closeServerStreams(Socket socket) throws IOException{
         fromClient.close();
         toClient.close();
-        server_socket.close();//a change here
+        socket.close();
         serverSocket.close();
    }
-    public void clientChat() throws IOException{
-        new ClientThread().t2.start();
+   public void openClientStreams(Socket socket, int port) throws UnknownHostException, IOException{
+        //client_socket = new Socket("127.0.0.1",port);
+        toServer = new PrintWriter(socket.getOutputStream(),true);
+        fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     }
-    
-    public void serverChat() throws IOException{
-        new ServerThread().t.start();
-    }
-    
-    class ServerThread implements Runnable{
-        Thread t;
-        public ServerThread(){
-            t = new Thread(this, "SevrerThread");
-            //t.start();
-        }
-        synchronized public void toClient(){
-            String reply;
-            Scanner sc;
-            sc = new Scanner(System.in);
-            System.out.println("Server : ");
-            reply = sc.nextLine();
-            toClient.print(reply);
-        }
-        synchronized public void fromClient(){
-            String msg;
-            //while (true){
-                try {
-                    msg = fromClient.readLine();
-                    System.out.println("Client : "+msg);
-                } catch (IOException ex) {
-                    Logger.getLogger(ChatFunctions.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            //}
-        }
-
-        @Override
-        public void run() {
-            while(true){
-                this.toClient();
-                this.fromClient();
-            }
-        }
-    }
-    
-    class ClientThread implements Runnable{
-        Thread t2;
-        public ClientThread(){
-            t2 = new Thread(this, "ClientThread");
-        }
-        synchronized public void toServer(){
-            String msg;
-            Scanner scanner;
-            System.out.print("You : ");
-            scanner = new Scanner(System.in);
-            msg = scanner.nextLine();
-            toServer.println(msg);
-            
-        }
-        synchronized public void fromServer(){
-            try {
-                    String tmp = fromServer.readLine();
-                    System.out.println("Server : " + tmp);
-                } catch (IOException ex) {
-                    Logger.getLogger(ChatFunctions.class.getName()).log(Level.SEVERE, null, ex);
-                }
-        }
-
-        @Override
-        public void run() {
-            while(true){
-                this.toServer();
-                this.fromServer();
-            }
-        }
-    }
+    public void closeClientStreams(Socket socket) throws IOException{
+        fromServer.close();
+        toServer.close();
+        socket.close();
+    }   
 }
+
